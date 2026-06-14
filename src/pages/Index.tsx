ace556d12deaaf680b2a2ce666e730bee28b1877
evertonltaborda1978-collab <evertonltaborda1978@gmail.com>
2026-06-14 14:14:23 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -32,7 +32,7 @@ const DEVICE_LABEL_PROMPT_KEY = "device_label_prompt_dismissed";
 
 const Index = () => {
   const [tab, setTab] = useState<Tab>("notes");
-  const { notes, addNote, deleteNote, restoreNote, permanentDeleteNote, emptyTrash, updateNote, setNoteReminder, lockNoteWithPin, unlockNoteWithPin, verifyNotePin, syncStatus, draftCount, exportBackup, importBackup, shouldRemindBackup, reminderAlert, dismissReminderAlert, snoozeReminderAlert, trashedNotes } = useNotes();
+  const { notes, addNote, deleteNote, restoreNote, permanentDeleteNote, emptyTrash, updateNote, setNoteReminder, togglePinNote, lockNoteWithPin, unlockNoteWithPin, verifyNotePin, syncStatus, draftCount, exportBackup, importBackup, shouldRemindBackup, reminderAlert, dismissReminderAlert, snoozeReminderAlert, trashedNotes } = useNotes();
   const { appointments, trashedAppointments, addAppointment, updateAppointment, deleteAppointment, restoreAppointment, permanentDeleteAppointment, emptyAppointmentTrash, activeAlert, dismissAlert, snoozeAlert } = useAppointments();
   const { signOut } = useAuth();
   const { currentDevice, fetchDevices } = useDeviceTracking();
@@ -114,6 +114,7 @@ const Index = () => {
             onDelete={deleteNote}
             onUpdate={updateNote}
             onSetReminder={setNoteReminder}
+            onTogglePin={togglePinNote}
             onLockNote={lockNoteWithPin}
             onUnlockNote={unlockNoteWithPin}
             onVerifyPin={verifyNotePin}
